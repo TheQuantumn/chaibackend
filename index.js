55f267fa3b9a4594ce709a852d4cb031a0ed51c3
Priyanshu Gupta <108951039+TheQuantumn@users.noreply.github.com>
2023-12-20 12:41:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/me', (req, res) => {
+  res.send('Fk u!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
